7277672a34c1153fc94f1ac2090d2b7f713abf09
CaitlinLandrus <caitlin.landrus@outlook.com>
2020-03-11 20:03:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 //used youtube.com/watch?v=NX3jqtwfzVY as reference on the path and react side
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .set('views', path.join(__dirname, 'views'))
+  .set('build', path.join(__dirname, 'index.html'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('build/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
