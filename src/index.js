0418cce95baa0428a942a88b7c9f93f153425999
CaitlinLandrus <caitlin.landrus@outlook.com>
2020-03-11 19:54:10 -0500

    server.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//const express = require('express');
+//const path = require('path');
+//const app = express();
+
+//app.use(express.static(path.join(__dirname), 'public'))
+//this displays the contents defined in app.js onto the element 'root' which is in index.html
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
