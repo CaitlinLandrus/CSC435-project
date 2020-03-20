620b2faeb781f736df294b5afd135c62008dd57c
CaitlinLandrus <caitlin.landrus@outlook.com>
2020-03-19 19:53:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express();
 
 //used youtube.com/watch?v=NX3jqtwfzVY as reference on the path and react side
-app.use(express.static(path.join(__dirname, 'build')));
-app.set('build', path.join(__dirname, 'index.html'));
-app.set('view engine', 'ejs');
-//https://www.freecodecamp.org/news/deploy-a-react-node-app-to/
-app.get('/*', (req, res) => {
+express()
+  .use(express.static(path.join(__dirname, 'build')))
+  .set('build', path.join(__dirname, 'index.html'))
+  .set('view engine', 'ejs')
+  //https://www.freecodecamp.org/news/deploy-a-react-node-app-to/
+  .get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
