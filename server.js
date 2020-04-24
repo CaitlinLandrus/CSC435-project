065c15dd23d5ed5d4898466dd1e3569e5444e827
CaitlinLandrus <caitlin.landrus@outlook.com>
2020-04-24 16:30:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080
 const routes = require('./routes')
 
 //Connect DB
-mongoose.connect( ' mongodb://heroku_vqxcqjgz:munoujtev3548qjo6rmk10uk7r@ds139844.mlab.com:39844/heroku_vqxcqjgz'
+mongoose.connect( 'mongodb://heroku_vqxcqjgz:munoujtev3548qjo6rmk10uk7r@ds139844.mlab.com:39844/heroku_vqxcqjgz'
                 || 'mongodb://localhost/applicationdb', {
     useNewUrlParser: true
 });
